b8c30f5bca36c431718ccdb5b77b93dc9c57ac03
Isael Santos <isael@offertech.com.br>
2025-04-26 02:05:41 -0300

    CCF - Refactors guest form image handling
    Updates the guest form to use a base64 image upload component for handling guest images.
    This change simplifies the image handling process by removing the tabbed interface and directly integrating the base64 upload field.
    
    The image data is now stored as a base64 string, streamlining the submission process.
    Also improves the user experience for image management.

--- src/components/ImageUploadField.js
+++ src/components/ImageUploadField.js
@@ -367,7 +367,7 @@ const ImageUploadField = ({
           </Paper>
         )}
       </FormControl>
-            
+
       {/* Snackbar para feedback */}
       <Snackbar
         open={snackbarOpen}
